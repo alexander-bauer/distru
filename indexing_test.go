6f222d5be812d22753029e4b6f34bbea47e7b694
Sasha Crofter <sasha@crofter.org>
2012-09-24 21:19:55 -0400

    added indexing_test.go, with an old test in it

---
+++ indexing_test.go
@@ -0,0 +1,11 @@
+package distru
+
+/*
+import "testing"
+
+func TestChunk(t *testing.T) {
+	chunk := NewChunk("localhost")
+	if chunk.(T) != &Chunk {
+		t.Errorf("NewChunk(url) did not make a new Chunk.")
+	}
+}*/
