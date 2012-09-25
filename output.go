e61aa20d3621cdf84c2ff7dae06b9e6d60787ce9
Sasha Crofter <sasha@crofter.org>
2012-09-25 18:57:03 -0400

    fixed up Index representation in RepIndex()

--- output.go
+++ output.go
@@ -6,5 +6,14 @@ package main
 }*/
 
 func RepIndex(index *Index) string {
-	return "Got here."
+	s := "Index:\n"
+	
+	for i := 0; i < len(index.Sites); i++ {
+		s += index.Sites[i].URL + "\n"
+		for j := 0; j < len(index.Sites[i].Pages); j++ {
+			s += "\t" + index.Sites[i].Pages[j].Path + "\n"
+		}
+	}
+	
+	return s + "\n\n"
 }
