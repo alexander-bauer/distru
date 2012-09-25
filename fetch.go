8f4b593bd8e1488f8c616a55060798aeddcd9ffa
Luke Evers <lukevers@gmail.com>
2012-09-25 19:51:33 -0400

    separated internal and external links. created two functions getInternalLinks and getExternalLinks

--- fetch.go
+++ fetch.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"regexp"
+	"strings"
 )
 
 //fetch a webpage from url (without http://)
@@ -45,12 +46,62 @@ func getLinks(html string) []string {
 	return (linkTexts)
 }
 
+func getInternalLinks(links []string, s string) []string {
+	in := [999]string{} //eventually we need to change the "999"
+	ixcount := 0
+
+	for i := range links {
+		if strings.Contains(s, links[i]) && !strings.Contains(links[i], "http://") {
+			in[ixcount] = links[i]
+			ixcount++
+		}
+	}
+
+	//this part gets rid of the extra 900 or so spaces that are there
+	internal := make([]string, ixcount)
+	for i := 0; i < ixcount; i++ {
+		internal[i] = in[i]
+	}
+	return (internal)
+}
+
+func getExternalLinks(links []string, s string) []string {
+	ex := [999]string{} //eventually we need to change the "999"
+	excount := 0
+
+	for i := range links {
+		if strings.Contains(links[i], "http://") {
+			ex[excount] = links[i]
+			excount++
+		}
+	}
+
+	//this part gets rid of the extra 900 or so spaces that are there
+	external := make([]string, excount)
+	for i := 0; i < excount; i++ {
+		external[i] = ex[i]
+	}
+	return (external)
+}
+
 func main() {
 	s := fetch(os.Args[1])
 	links := getLinks(s)
 
-	for i := range links {
-		print(links[i], "\n")
+	internal := getInternalLinks(links, s)
+	external := getExternalLinks(links, s)
+
+	print("Internal Links:\n")
+	for i := range internal {
+		print(internal[i], "\n")
 	}
+
+	print("\n")
+
+	print("External Links:\n")
+	for i := range external {
+		print(external[i], "\n")
+	}
+
 	os.Exit(0)
 }
