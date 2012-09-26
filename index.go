8c2f627a88cf4d35fb9173277cdde9fffc16113b
Sasha Crofter <sasha@crofter.org>
2012-09-26 00:57:31 -0400

    pulled internal link gathering from treebuilding, but not the bad code

--- index.go
+++ index.go
@@ -9,11 +9,13 @@ type Index struct {
 type site struct {
 	URL   string     //the link that identifies this Block
 	Pages []sitePage //nonordered list of pages and their data on the server
+	Tree []string
 }
 
 type sitePage struct {
 	Path    string   //path to page on the webserver (relative to root page)
 	Links   []string //list of hyperlinks on the page
+	Internals []string //list of internal links on the page
 	Content string   //the content, temporarily replacing word lists
 }
 
@@ -43,29 +45,35 @@ func (index *Index) save(path string) error {
 
 
 func newSite(url string) *site {
-	pages := []sitePage{*newSitePage(url)} //make an array of length 1
+	pages := []sitePage{*newSitePage(url, "/")} //make an array of length 1
 	//by scraping the site's page
 	//TODO this should build the whole tree
 
 	site := site{
 		URL:   url,
 		Pages: pages,
+		Tree: pages[0].Internals, //just grabbing /'s internal links
 	}
 	return &site
 }
 
-//sitePage constructor, which scrapes a single webpage
-func newSitePage(url string) *sitePage {
+//newSitePage is the sitePage constructor, which scrapes a single webpage
+//it takes the URL of a site (without trailing /,) and a directory path, such
+//as / or /help.txt
+//It returns the sitePage object, as well as an array of internal links.
+func newSitePage(url string, path string) *sitePage {
 	body := fetch(url)      //get the body of the webpage
 	allLinks := getLinks(body) //collect links, but
-	links := getExternalLinks(allLinks) //get only the external links
+	externalLinks := getExternalLinks(allLinks) //get only the external links
+	internalLinks := getInternalLinks(allLinks, body) //get only internal links
 
 	//the wordlist should be added here, but that function doesn't exist yet
 	//TODO
 
 	page := sitePage{
-		Path:    url,
-		Links:   links,
+		Path:    path,
+		Links:   externalLinks,
+		Internals: internalLinks,
 		Content: body,
 	}
 	return &page
