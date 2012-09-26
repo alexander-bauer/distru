29e0e0d0d2031e2263cf37ed35a9f7859631afab
Sasha Crofter <sasha@crofter.org>
2012-09-25 22:20:08 -0400

    moved main out of fetch.go

--- fetch.go
+++ fetch.go
@@ -83,25 +83,3 @@ func getExternalLinks(links []string) []string {
 	}
 	return (external)
 }
-
-func main() {
-	s := fetch(os.Args[1])
-	links := getLinks(s)
-
-	internal := getInternalLinks(links, s)
-	external := getExternalLinks(links)
-
-	print("Internal Links:\n")
-	for i := range internal {
-		print(internal[i], "\n")
-	}
-
-	print("\n")
-
-	print("External Links:\n")
-	for i := range external {
-		print(external[i], "\n")
-	}
-
-	os.Exit(0)
-}
