f5c1fa5adc21d87799cb7d0be38855717e37e4bf
Luke Evers <me@lukevers.com>
2012-10-24 17:10:49 -0400

    started building the index page, updated some of the webui files

--- web.go
+++ web.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io/ioutil"
 )
 
 func ServeWeb() {
@@ -13,7 +14,56 @@ func ServeWeb() {
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-	s := "placeholder"
+	//add the <html> element
+	s := "<html>"
+	fmt.Fprint(w, s)
+	
+	//add the <head> element
+	s = "<head>"
+	fmt.Fprint(w, s)
+	
+	//add the stylesheet
+	s = "<style type='text/css'>"
+	fmt.Fprint(w, s)
+	file, err := ioutil.ReadFile("webui/style.css")
+		if err != nil { panic(err) }
+	s = string(file)
+	fmt.Fprint(w, s)
+	s = "</style>"
+	fmt.Fprint(w, s)
+	
+	//add the jquery
+	s = "<script type='text/javscript'>"
+	fmt.Fprint(w, s)
+	file, err = ioutil.ReadFile("webui/jquery.js")
+		if err != nil { panic(err) }
+	s = string(file)
+	fmt.Fprint(w, s)
+	s = "</script>"
+	fmt.Fprint(w, s)
+	
+	//add the javascript file
+	s = "<script type='text/javscript'>"
+	fmt.Fprint(w, s)
+	file, err = ioutil.ReadFile("webui/common.js")
+		if err != nil { panic(err) }
+	s = string(file)
+	fmt.Fprint(w, s)
+	s = "</script>"
+	fmt.Fprint(w, s)
+		
+	//close the <head> element
+	s = "</head>"
+	fmt.Fprint(w, s)
+	
+	//add the body of index.html
+	file, err = ioutil.ReadFile("webui/index.html")
+		if err != nil { panic(err) }
+	s = string(file)
+	fmt.Fprint(w, s)
 
-	fmt.Fprintf(w, s)
+	//close the <html> element
+	s = "</html>"
+	fmt.Fprint(w, s)
+	
 }
