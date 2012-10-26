4ab91f8e3711b5b973a3a2b2dcf940bc2ab786ce
Sasha Crofter <sasha@crofter.org>
2012-10-26 19:41:27 -0400

    added exiting on build fail on run.sh
    I added a clause in run.sh that causes it to exit if the "go build"
    command fails for any reason. I also added an argument which causes it
    to execute "tail -f log" if the first argument to the script is "log".

--- run.sh
+++ run.sh
@@ -6,12 +6,22 @@ cd $(dirname $0)
 
 LOG=log
 
+if [[ $1 == "log" ]]
+then
+	AUTOTAIL=TRUE
+fi
+
 if [[ $(pgrep distru | wc -l) != 0 ]]
 then
 	killall distru
 fi
 
 go build
+if [[ $? != 0 ]]
+then
+	exit
+fi
+
 cat /dev/null > $LOG
 ./distru &> $LOG &
 
