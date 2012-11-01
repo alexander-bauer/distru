d73aae9ef7f281e5db92e04fa4812c67a1becb47
Sasha Crofter <sasha@crofter.org>
2012-11-01 19:25:09 -0400

    minor codestyle fixes
    I removed the irrelevant os.Exit(1) at the end of main(), and thereby
    removed the "os" import.

--- distru.go
+++ distru.go
@@ -1,10 +1,7 @@
 package main
 
-import "os"
-
 var Idx = &Index{Sites: make(map[string]site)}
 
 func main() {
 	Serve()
-	os.Exit(1)
 }
--- distru_test.go
+++ distru_test.go
@@ -4,18 +4,6 @@ import (
 	"testing"
 )
 
-func TestBin(t *testing.T) {
-	idx := RecvIndex("localhost")
-	t.Log("Got index from localhost.")
-	t.Log(idx.JSON())
+func ServerCheck(t *testing.T) {
+
 }
-
-/*func TestRep(t *testing.T) {
-	print(RepIndex(Idx))
-}*/
-
-/*
-func TestSave(t *testing.T) {
-	index := NewIndex()
-	index.save("/tmp/distru-index")
-}*/
