c5eef9bdfe8d1e80d6f67b86e0034b2fd83afc67
Luke Evers <me@lukevers.com>
2012-11-02 14:14:47 -0400

    Fixed a spacing problem in the comments of distru.js

--- ui/distru.js
+++ ui/distru.js
@@ -1,19 +1,19 @@
-//	The function searchThis() works with Safari and Chrome
-//	but not with FireFox and Opera. Instead of using a 
-//	complete HTML5 form, we use an onkeydown function that
-//	calls the javascript function to redirect us to search/x
-//	where x is the search term(s)
+//  The function searchThis() works with Safari and Chrome
+//  but not with FireFox and Opera. Instead of using a 
+//  complete HTML5 form, we use an onkeydown function that
+//  calls the javascript function to redirect us to search/x
+//  where x is the search term(s)
 function searchThis() {
 	if (event.keyCode == 13) {
 		window.location += 'search/'+document.getElementById('search').value;
 	}
 }
 
-//	The function isEnter(e) works with FireFox and Opera
-//	while the other function works with Safari and Chrome.
-//	Instead of using a complete HTML5 form, we use an onkeypress
-//	function that calls the javascript function to redirect
-//	us to search/x where x is the search term(s)
+//  The function isEnter(e) works with FireFox and Opera
+//  while the other function works with Safari and Chrome.
+//  Instead of using a complete HTML5 form, we use an onkeypress
+//  function that calls the javascript function to redirect
+//  us to search/x where x is the search term(s)
 function isEnter(e) {
     e = e || window.event || {};
     var charCode = e.charCode || e.keyCode || e.which;
