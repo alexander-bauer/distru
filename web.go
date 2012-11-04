d7d736198fd8d400bfa49e06ae47d7d6d26ad7fe
Luke Evers <me@lukevers.com>
2012-11-04 12:03:37 -0500

    Removed the use of fmt package and rewrote web.go. Closes #26.

--- web.go
+++ web.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,40 +18,51 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	//get the search term and save it as searchTerm
 	searchTerm := r.URL.Path[len("/search/"):]
 	//get the number of results for the searchTerm
-	numResults := 0
+	numResults := 2
 	log.Println("<-" + r.RemoteAddr + "> searching \"" + searchTerm + "\"")
 	
-	//add the page
-	fmt.Fprintf(w, "<html><head><title>Distru :: Searching \"%s\"</title><style type=\"text/css\">", searchTerm)
-	file, err := ioutil.ReadFile("ui/search.css")
+	//load external files
+	css, err := ioutil.ReadFile("ui/search.css")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprint(w, string(file))
-	fmt.Fprintf(w, "</style></head><body><div class=\"searchterm\">%d results for <strong>%s</strong></div>", numResults, searchTerm)
-
+	
+	//add the page
+	w.Write([]byte("<html><head><title>Distru :: Searching "))
+	w.Write([]byte(searchTerm))
+	w.Write([]byte("</title><style type=\"text/css\">"))
+	w.Write(css)
+	w.Write([]byte("</style></head><body><div class=\"searchterm\">"))
+	w.Write([]byte(string(numResults)))
+	w.Write([]byte(" results for <strong>"))
+	w.Write([]byte(searchTerm))
+	w.Write([]byte("</strong></div>"))
+	
 	//TODO: SEARCH HERE.
-	//this is a temporary example of what searches will look like
-	fmt.Fprint(w, "<div class=\"results\">test</div>")
-	fmt.Fprint(w, "<div class=\"results\">test2</div>")
+	//THIS IS JUST AN EXAMPLE..
+	w.Write([]byte("<div class=\"results\">test</div>"))
 	
 	//close page
-	fmt.Fprint(w, "</body></html>")
+	w.Write([]byte("</body></html>"))
 }
 
 func frontpageHandler(w http.ResponseWriter, r *http.Request) {
+	//load external files
+	css, err := ioutil.ReadFile("ui/index.css")
+	if err != nil {
+		panic(err)
+	}
+	javascript, err := ioutil.ReadFile("ui/distru.js")
+	if err != nil {
+		panic(err)
+	}
+	
 	//add the page
-	fmt.Fprint(w, "<html><head><title>Distru :: Search Freely</title><style type=\"text/css\">")
-	file, err := ioutil.ReadFile("ui/index.css")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Fprint(w, string(file))
-	fmt.Fprint(w, "</style><script type=\"text/javascript\">")
-	file, err = ioutil.ReadFile("ui/distru.js")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Fprint(w, string(file))
-	fmt.Fprintf(w, "</script></head><body><div class = \"version\">Version %s</div><div class = \"name\">Distru</div><input type=\"text\" onkeydown=\"searchThis();\" onkeypress=\"isEnter(event);\" id=\"search\" class=\"search\" placeholder=\"Search freely\"/></body></html>", Version)
+	w.Write([]byte("<html><head><title>Distru :: Search Freely</title><style type=\"text/css\">"))
+	w.Write(css)
+	w.Write([]byte("</style><script type=\"text/javascript\">"))
+	w.Write(javascript)
+	w.Write([]byte("</script></head><body><div class = \"version\">"))
+	w.Write([]byte(Version))
+	w.Write([]byte("<div class = \"name\">Distru</div><input type=\"text\" onkeydown=\"searchThis();\" onkeypress=\"isEnter(event);\" id=\"search\" class=\"search\" placeholder=\"Search freely\"/></body></html>"))
 }
