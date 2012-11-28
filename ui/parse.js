15f201594402d7823a7d16f8afc484871b02b8a3
Luke Evers <me@lukevers.com>
2012-11-28 17:17:25 -0500

    Fixed problem where if there were lots of spaces or no spaces at all, it wouldn't do math.
    Ref #72

--- ui/parse.js
+++ ui/parse.js
@@ -43,19 +43,25 @@ function doMath(term) {
 		
 		
 		if (isHex(term)){
-			var original = term;		
+			var original = term;
+			term = term.replace("+", " + ");
+			term = term.replace("-", " - ");
+			term = term.replace("*", " * ");
+			term = term.replace("/", " / ");
+			term = term.replace("%", " % ");
+			
+			term = term.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
+			
 			var array = term.split(" ");
+			
 			
 			for (var i = 0; i < array.length; i++) {
 				if (!(array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "%")) {
 					array[i] = parseInt(array[i], 16);
 				}
 			}
-			term = array.join(' ');
-			alert(term);
-			
+			term = array.join(' ');			
 			var value = eval(term);
-			
 			value = value.toString(16);
 			
 			document.getElementById("blank").innerHTML = "<center><div class='calculate' onmouseover='unhideBubble();' onmouseout='hideBubble();'>" + original + " = <strong>0x" + value + "</strong></div><div class='bubble'><strong>What's this?</strong><br/>What you serached seemed to us like it was math, so we did the math for you!</div></center>";
