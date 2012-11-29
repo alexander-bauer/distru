bd72ec75fe97057474cb5dd17a64aa0aa6e4b470
Luke Evers <me@lukevers.com>
2012-11-28 20:21:47 -0500

    Reduced the size of parse.js.
    Knowingly, I had a lot of extra code that didn't need to be duplicated,
    but it was. I was only doing that at first just to test each part, but
    now I know it's safe to put it all together. So now there is less code,
    but it does the same thing.

--- ui/parse.js
+++ ui/parse.js
@@ -13,18 +13,17 @@ var term = url.substring(url.indexOf(":9048/search/")+9);
 // is a mathematical equation, then do the math and add a
 // div on the search page with the answer for the user.
 function parse() {
-	doMath(term);
-	
+	parseMath(term);
 }
 
-// The function doMath(term) checks to see if the term
+// The function parseMath(term) checks to see if the term
 // is a math equation or not. First, it should parse
 // words and change them into the corresponding symbols. 
-// Ex: plus, minus, times ... +, -, *
+// Ex: plus, minus, times, ... TO ... +, -, *, ...
 // Then it should figure out if it is a real equation or not.
 // If it is a real equation, do the math, and if it is
 // not a real equation, then don't do the math.
-function doMath(term) {
+function parseMath(term) {
 	try {
 		
 		// put it in lowercase so we don't have to deal
@@ -41,102 +40,15 @@ function doMath(term) {
 		
 		
 		if (isHex(term)){
-			var original = term;
-			term = term.replace("+", " + ");
-			term = term.replace("-", " - ");
-			term = term.replace("*", " * ");
-			term = term.replace("/", " / ");
-			term = term.replace("%", " % ");
-			
-			term = term.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
-			
-			var array = term.split(" ");
-			
-			
-			for (var i = 0; i < array.length; i++) {
-				if (!(array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "%")) {
-					if (array[i].length > 2 && array[i].substring(0,2) == "0o") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(temp, 16);
-					}
-					else if (array[i].length > 2 && array[i].substring(0,2) == "0b") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(temp, 16);
-					}
-					else array[i] = parseInt(array[i], 16);
-				}
-			}
-			term = array.join(' ');			
-			var value = eval(term);
-			value = value.toString(16);
-			
-			document.getElementById("blank").innerHTML = "<center><div class='calculate' onmouseover='unhideBubble();' onmouseout='hideBubble();'>" + original + " = <strong>0x" + value + "</strong></div><div class='bubble'><strong>What's this?</strong><br/>What you serached seemed to us like it was math, so we did the math for you!</div></center>";
+			doMath(16, "0x", term);
 		} //close hex
 		
 		else if (isOctal(term)) {
-			var original = term;
-			term = term.replace("+", " + ");
-			term = term.replace("-", " - ");
-			term = term.replace("*", " * ");
-			term = term.replace("/", " / ");
-			term = term.replace("%", " % ");
-			
-			term = term.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
-			
-			var array = term.split(" ");
-			
-			
-			for (var i = 0; i < array.length; i++) {
-				if (!(array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "%")) {
-					if (array[i].length > 2 && array[i].substring(0,2) == "0o") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(temp, 8);
-					}
-					else if (array[i].length > 2 && array[i].substring(0,2) == "0b") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(temp, 2);
-					}
-					else array[i] = parseInt(array[i], 8);
-				}
-			}
-			term = array.join(' ');			
-			var value = eval(term);
-			value = value.toString(8);
-			
-			document.getElementById("blank").innerHTML = "<center><div class='calculate' onmouseover='unhideBubble();' onmouseout='hideBubble();'>" + original + " = <strong>0o" + value + "</strong></div><div class='bubble'><strong>What's this?</strong><br/>What you serached seemed to us like it was math, so we did the math for you!</div></center>";
+			doMath(8, "0o", term);
 		} //close octal
 		
 		else if (isBinary(term)) {
-			var original = term;
-			term = term.replace("+", " + ");
-			term = term.replace("-", " - ");
-			term = term.replace("*", " * ");
-			term = term.replace("/", " / ");
-			term = term.replace("%", " % ");
-			
-			term = term.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
-			
-			var array = term.split(" ");
-			
-			
-			for (var i = 0; i < array.length; i++) {
-				if (!(array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "%")) {
-					if (array[i].length > 2 && array[i].substring(0,2) == "0b") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(temp, 2);
-					}
-					else if (array[i].length > 2 && array[i].substring(0,2) == "0o") {
-						var temp = array[i].substring(2);
-						array[i] = parseInt(array[i], 2);
-					}
-					else array[i] = parseInt(array[i], 2);
-				}
-			}
-			term = array.join(' ');			
-			var value = eval(term);
-			value = value.toString(2);
-			
-			document.getElementById("blank").innerHTML = "<center><div class='calculate' onmouseover='unhideBubble();' onmouseout='hideBubble();'>" + original + " = <strong>0b" + value + "</strong></div><div class='bubble'><strong>What's this?</strong><br/>What you serached seemed to us like it was math, so we did the math for you!</div></center>";
+			doMath(2, "0b", term);
 		} //close binary
 		
 		else
@@ -152,28 +64,69 @@ function doMath(term) {
 	catch (e) {}
 }
 
+// The function doMath(base, type, newterm) does math!
+// More specifically, it does hex, octal, and binary 
+// mathematical equations. 
+function doMath(base, type, newterm) {
+	var original = newterm;
+	term = newterm;
+	term = term.replace("+", " + ");
+	term = term.replace("-", " - ");
+	term = term.replace("*", " * ");
+	term = term.replace("/", " / ");
+	term = term.replace("%", " % ");
+	
+	term = term.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
+	
+	var array = term.split(" ");
+	
+	
+	for (var i = 0; i < array.length; i++) {
+		if (!(array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "%")) {
+			if (array[i].length > 2 && array[i].substring(0,2) == "0o" || array[i].substring(0,2) == "0b") {
+				var temp = array[i].substring(2);
+				array[i] = parseInt(temp, base);
+			}
+			else array[i] = parseInt(array[i], base);
+		}
+	}
+	term = array.join(' ');			
+	var value = eval(term);
+	value = value.toString(base);
+	
+	document.getElementById("blank").innerHTML = "<center><div class='calculate' onmouseover='unhideBubble();' onmouseout='hideBubble();'>" + original + " = <strong>" + type + value + "</strong></div><div class='bubble'><strong>What's this?</strong><br/>What you serached seemed to us like it was math, so we did the math for you!</div></center>";
+}
+
+// The function isHex() determines if 
+// it is a hex number or not.
 function isHex() {
 	if (term.indexOf("0x") !== -1)
 		return true;
 	else return false;
 }
 
+// The function isOctal() determines if
+// it is an octal number or not
 function isOctal() {
 	if (term.indexOf("0o") !== -1)
 		return true;
 	return false;
 }
 
+// The function isBinary() determines if
+// it is a binary number or not
 function isBinary() {
 	if (term.indexOf("0b") !== -1)
 		return true;
 	return false;
 }
 
+// The function unhideBubble() unhides the bubble!
 function unhideBubble() {
 	document.getElementsByClassName("bubble").item(0).style.opacity = "1";
 }
 
+// The function hideBubble() hides the bubble!
 function hideBubble() {
 	document.getElementsByClassName("bubble").item(0).style.opacity = "0";
 }
